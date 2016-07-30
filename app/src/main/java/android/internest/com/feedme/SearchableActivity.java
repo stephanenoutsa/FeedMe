61009017c5b897490397fb4aafdf6f6e38d7bfd4
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-07-30 17:01:27 +0100

    Creates the SearchableActivity to handle search queries

---
+++ app/src/main/java/android/internest/com/feedme/SearchableActivity.java
@@ -0,0 +1,39 @@
+package android.internest.com.feedme;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        // Get the intent, verify the action and get the query
+        Intent i = getIntent();
+        if (Intent.ACTION_SEARCH.equals(i.getAction())) {
+            String query = i.getStringExtra(SearchManager.QUERY);
+            //doMySearch(query);
+        }
+    }
+
+}
